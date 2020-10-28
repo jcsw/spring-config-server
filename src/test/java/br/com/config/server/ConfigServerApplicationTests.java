b7be0481f48662e6860a4a50d1b366dc72a88c8d
Jonas Almeida Cezar <jcezar.al@gmail.com>
2020-10-28 19:09:56 -0300

    first commit

---
+++ src/test/java/br/com/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplication {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
